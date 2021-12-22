23d1c2f394c4cbd966e5e6cb574c452bfe74a7b1
tolikavr <anatolii13@mail.ru>
2021-12-22 12:24:13 +0300

    Add fragment to MainActivity.kt

--- app/src/main/java/com/tolikavr/shoppinglist/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/tolikavr/shoppinglist/presentation/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tolikavr.shoppinglist.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -9,15 +11,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.tolikavr.shoppinglist.R
 import com.tolikavr.shoppinglist.presentation.adapter.ShopListAdapter
 import com.tolikavr.shoppinglist.presentation.ui.shopitem.ShopItemActivity
+import com.tolikavr.shoppinglist.presentation.ui.shopitem.ShopItemFragment
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var viewModel: MainViewModel
   private lateinit var shopListAdapter: ShopListAdapter
+  private var shopItemContainer: FragmentContainerView? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    shopItemContainer = findViewById(R.id.shop_item_container)
 
     setupRecyclerView()
 
@@ -30,9 +35,25 @@ class MainActivity : AppCompatActivity() {
     val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
 
     buttonAddItem.setOnClickListener {
-      val intent = ShopItemActivity.newIntentAddItem(this)
-      startActivity(intent)
+      if (isOnePaneMode()) {
+        val intent = ShopItemActivity.newIntentAddItem(this)
+        startActivity(intent)
+      } else {
+        launchFragment(ShopItemFragment.newInstanceAddItem())
+      }
     }
+  }
+
+  private fun isOnePaneMode(): Boolean {
+    return shopItemContainer == null
+  }
+
+  private fun launchFragment(fragment: Fragment) {
+    supportFragmentManager.popBackStack()
+    supportFragmentManager.beginTransaction()
+      .replace(R.id.shop_item_container, fragment)
+      .addToBackStack(null)
+      .commit()
   }
 
   private fun setupRecyclerView() {
@@ -70,8 +91,12 @@ class MainActivity : AppCompatActivity() {
 
   private fun setupClickListener() {
     shopListAdapter.onShopItemClickListener = { shopItem ->
-      val intent = ShopItemActivity.newIntentEditItem(this, shopItem.id)
-      startActivity(intent)
+      if (isOnePaneMode()) {
+        val intent = ShopItemActivity.newIntentEditItem(this, shopItem.id)
+        startActivity(intent)
+      } else {
+        launchFragment(ShopItemFragment.newInstanceEditItem(shopItem.id))
+      }
     }
   }
 
--- app/src/main/java/com/tolikavr/shoppinglist/presentation/ui/shopitem/ShopItemActivity.kt
+++ app/src/main/java/com/tolikavr/shoppinglist/presentation/ui/shopitem/ShopItemActivity.kt
@@ -3,27 +3,13 @@ package com.tolikavr.shoppinglist.presentation.ui.shopitem
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputLayout
 import com.tolikavr.shoppinglist.R
 import com.tolikavr.shoppinglist.domain.model.ShopItem
 import com.tolikavr.shoppinglist.presentation.ui.shopitem.ShopItemFragment.Companion.newInstanceAddItem
 import com.tolikavr.shoppinglist.presentation.ui.shopitem.ShopItemFragment.Companion.newInstanceEditItem
 
 class ShopItemActivity : AppCompatActivity() {
-
-//  private lateinit var viewModel: ShopItemViewModel
-
-//  private lateinit var tilName: TextInputLayout
-//  private lateinit var tilCount: TextInputLayout
-//  private lateinit var etName: TextView
-//  private lateinit var etCount: TextView
-//  private lateinit var buttonSave: Button
 
   private var screenMode = MODE_UNKNOWN
   private var shopItemId = ShopItem.UNDEFINED_ID
@@ -32,87 +18,21 @@ class ShopItemActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_shop_item)
     parseIntent()
-//    viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//    initViews()
-//    addTextChangeListeners()
-    launchRightMode()
-//    observeViewModel()
+    if (savedInstanceState == null)
+      launchRightMode()
   }
 
-//  private fun observeViewModel() {
-//    viewModel.errorInputName.observe(this) {
-//      val message = if (it) {
-//        getString(R.string.error_input_name)
-//      } else {
-//        null
-//      }
-//      tilName.error = message
-//    }
-//
-//    viewModel.errorInputCount.observe(this) {
-//      val message = if (it) {
-//        getString(R.string.error_input_count)
-//      } else {
-//        null
-//      }
-//      tilCount.error = message
-//    }
-//
-//    viewModel.shouldCloseScreen.observe(this) {
-//      finish()
-//    }
-//  }
-//
   private fun launchRightMode() {
-    val fragment  = when (screenMode) {
+    val fragment = when (screenMode) {
       MODE_EDIT -> newInstanceEditItem(shopItemId)
       MODE_ADD -> newInstanceAddItem()
       else -> throw java.lang.RuntimeException("Unknown screen mode $screenMode")
     }
-  supportFragmentManager.beginTransaction()
-      .add(R.id.shop_item_container, fragment)
+    supportFragmentManager.beginTransaction()
+      .replace(R.id.shop_item_container, fragment)
       .commit()
   }
-//
-//  private fun addTextChangeListeners() {
-//    etName.addTextChangedListener(object : TextWatcher {
-//      override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//
-//      override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//        viewModel.resetErrorInputName()
-//      }
-//
-//      override fun afterTextChanged(s: Editable?) {}
-//    })
-//
-//    etCount.addTextChangedListener(object : TextWatcher {
-//      override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//
-//      override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//        viewModel.resetErrorInputCount()
-//      }
-//
-//      override fun afterTextChanged(s: Editable?) {}
-//    })
-//  }
-//
-//  private fun launchEditMode() {
-//    viewModel.getShopItem(shopItemId)
-//    viewModel.shopItem.observe(this, {
-//      etName.text = it.name
-//      etCount.text = it.count.toString()
-//    })
-//    buttonSave.setOnClickListener {
-//      viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
-//    }
-//  }
-//
-//  private fun launchAddMode() {
-//    buttonSave.setOnClickListener {
-//      viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
-//    }
-//  }
-//
+
   private fun parseIntent() {
     if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
       throw RuntimeException("Param screen mode is absent")
@@ -129,14 +49,6 @@ class ShopItemActivity : AppCompatActivity() {
       shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
     }
   }
-//
-//  private fun initViews() {
-//    tilName = findViewById(R.id.til_name)
-//    tilCount = findViewById(R.id.til_count)
-//    etName = findViewById(R.id.et_name)
-//    etCount = findViewById(R.id.et_count)
-//    buttonSave = findViewById(R.id.save_button)
-//  }
 
   companion object {
     private const val EXTRA_SCREEN_MODE = "extra_mode"
@@ -158,6 +70,4 @@ class ShopItemActivity : AppCompatActivity() {
       return intent
     }
   }
-
-
 }
